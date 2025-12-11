62f4a7d8756f7fd2b42a860c9b97c092b9fffde2
Kurniawan pratama <kurama.career@gmail.com>
2025-12-12 02:01:18 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,12 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
-import Components from "unplugin-vue-components/vite";
-import RadixVueResolver from "radix-vue/resolver";
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
-    Components({
-      dts: true,
-      resolvers: [RadixVueResolver()],
-    }),
   ],
 });
